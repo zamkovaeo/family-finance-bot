9c6f22b6fac9db6f58aa530c35fb9ecb96d67062
zamkovaeo <zamkova.eo@gmail.com>
2026-06-08 00:41:34 +0500

    Use Render provided port

--- app/__main__.py
+++ app/__main__.py
@@ -1,8 +1,9 @@
 import uvicorn
+import os
 
 from app.core.config import settings
 
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=settings.environment == "local")
-
+    port = int(os.getenv("PORT", "8000"))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=settings.environment == "local")
